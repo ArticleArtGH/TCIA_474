44bd28be8ff7aafb062a1d13b4388ab9136e0a7a
Chernykh Artyom <artemchernykh07@gmail.com>
2020-09-24 11:33:28 +0400

    final task 2 and 3

--- Задание 3 JS/scripts.js
+++ Задание 3 JS/scripts.js
@@ -8,10 +8,11 @@ function check(s){
 }
 
 function f1(){
+var num = 0;
 var div_w = "<div style='display:inline-block; width:100px; height:100px;"+
-"border:1px solid black; background-color:white;'></div>";
+"border:1px solid black; background-color:white; color:red;'>"+num+"</div>";
 var div_b = "<div style='display:inline-block; width:100px; height:100px;"+
-"border:1px solid black; background-color:black;'></div>";
+"border:1px solid black; background-color:black; color:red;'>"+(num++)+"</div>";
 var num_l = 8, num_r = 8;
 var arr_AH_t = new Array("A","B","C","D","E","F","G","H");
 var arr_AH_d = new Array("A","B","C","D","E","F","G","H");
@@ -25,31 +26,43 @@ for(var i = 0;i<4;i++){
         //     document.write("<br>");
         // }
         for(var j = 0; j<4;j++){
-            if(j == 0){
-                document.write("<div style='display:inline-block;'>"+num_l+"</div>",div_w,
-                div_b);
-                num_l--;
-            }else if(j==3){
-                document.write(div_w,div_b,"<div style='display:inline-block;'>"+num_r+
-                "</div>");
-                num_r--;
-            }else{
-                document.write(div_w,div_b);
-            }
+            // if(j == 0){
+            //     document.write("<div style='display:inline-block;'>"+num_l+"</div>",div_w,
+            //     div_b);
+            //     num_l--;
+            // }else if(j==3){
+            //     document.write(div_w,div_b,"<div style='display:inline-block;'>"+num_r+
+            //     "</div>");
+            //     num_r--;
+            // }else{
+                document.write("<div style='display:inline-block; width:100px; height:100px;"+
+                "border:1px solid black; background-color:white; color:red; text-align:center;"+
+                "vertical-align:middle;'><div style='display:flex; justify-content:center; align-items:center; width:100px; height:100px;'>"+(num++)+"</div></div>");
+                document.write("<div style='display:inline-block; width:100px; height:100px;"+
+                "border:1px solid black; background-color:black; color:red; text-align:center;"+
+                "vertical-align:middle;'><div style='display:flex; justify-content:center; align-items:center; width:100px; height:100px;'>"+(num++)+"</div></div>");
+                //num++;
+            //}
         }
         document.write("<br>");
         for(var j = 0;j<4;j++){
-            if(j==0){
-                document.write("<div style='display:inline-block;'>"+num_l+"</div>",div_b,
-                div_w);
-                num_l--;
-            }else if(j==3){
-                document.write(div_b,div_w,"<div style='display:inline-block;'>"+num_r+
-                "</div>");
-                num_r--;
-            }else{
-                document.write(div_b,div_w);
-            }
+            // if(j==0){
+            //     document.write("<div style='display:inline-block;'>"+num_l+"</div>",div_b,
+            //     div_w);
+            //     num_l--;
+            // }else if(j==3){
+            //     document.write(div_b,div_w,"<div style='display:inline-block;'>"+num_r+
+            //     "</div>");
+            //     num_r--;
+            // }else{
+                document.write("<div style='display:inline-block; width:100px; height:100px;"+
+                "border:1px solid black; background-color:black; color:red; text-align:center;"+
+                "vertical-align:middle;'><div style='display:flex; justify-content:center; align-items:center; width:100px; height:100px;'>"+(num++)+"</div></div>");
+                document.write("<div style='display:inline-block; width:100px; height:100px;"+
+                "border:1px solid black; background-color:white; color:red; text-align:center;"+
+                "vertical-align:middle;'><div style='display:flex; justify-content:center; align-items:center; width:100px; height:100px;'>"+(num++)+"</div></div>");
+                //num++;
+            //}
         }
         document.write("<br>");
         // if(i==3){
@@ -58,7 +71,7 @@ for(var i = 0;i<4;i++){
         //         document.write(arr_AH_d[i]);
         //     }
         // }
-    }  
+    }
 }
 function f2(){
     var min = check("Введите минимальное число таблицы: ");
@@ -69,9 +82,9 @@ function f2(){
     table +="<tr>";
     for(var j = 1; j<=10; j++){
         if(i == 1){
-        table += "<td>"+j+"</td>";
+        table += "<td style='width:20px; height:25px;'><div style='width:20px; height:25px; display:flex; justify-content:center; align-items:center;'>"+j+"</div></td>";
         }else{
-        table +="<td>"+Math.floor(Math.random()*(max-min)+min);+"</td>";
+        table +="<td style='width:20px; height:25px;'><div style='width:20px; height:25px; display:flex; justify-content:center; align-items:center;'>"+Math.floor(Math.random()*(max-min)+min);+"</div></td>";
         }
     }
     table +="</tr>";
