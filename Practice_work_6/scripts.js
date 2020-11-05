aac01ae5c8264188e08a57f95b1c4530cbe93759
Chernykh Artyom <artemchernykh07@gmail.com>
2020-11-05 16:35:52 +0300

    fix Pr_w 5 and 6

--- Practice_work_6/scripts.js
+++ Practice_work_6/scripts.js
@@ -1,3 +1,4 @@
+window.onload = function(){
 var arr;
 
 function check(s){
@@ -10,13 +11,13 @@ function check(s){
     }
 }
 
-function calc(){
+document.getElementById("btnCalc").onclick = function(){
     // var minV = check(document.getElementById("minV").value);
     // var maxV = check(document.getElementById("maxV").value);
     // var count = check(document.getElementById("count").value);
-    var minV = document.getElementById("minV").value;
-    var maxV = document.getElementById("maxV").value;
-    var count = document.getElementById("count").value;
+    var minV = Number(document.getElementById("minV").value);
+    var maxV = Number(document.getElementById("maxV").value);
+    var count = Number(document.getElementById("count").value);
     // minV=Number(minV);//check(minV));
     // maxV=Number(maxV);//check(maxV));
     // count = Number(count);//check(count));
@@ -33,17 +34,25 @@ function calc(){
     document.getElementById("sum").value=sum;    
 }
 
-function output(){
-    var table ="<table border='1px' cellspacing='0px' celpadding='5px'>";
+document.getElementById("btnOutput").onclick = function(){
+    var table = "<p>Вывод:</p>"; 
+    table +="<table border='1px' cellspacing='0px' celpadding='5px'>";
     
     table+="<tr>"
     for(var i = 0; i<arr.length;i++){
         table+="<td>"+(i+1)+"</td>";
-        table+="<td>"+arr[i]+"</td>";
     }
     table+="</tr>"
-
+    table+="<tr>"
+    for(var i = 0; i<arr.length;i++){  
+        table+="<td>"+arr[i]+"</td>";
+    }
+    
     //var d = "index.html";
     //d.write(table);
-    document.write(table);
+    //document.write(table);
+    // document.getElementById("pOut").innerHTML="Вывод:";
+    document.getElementById("out").innerHTML=table;
+}
+
 }
